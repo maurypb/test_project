595c2b8c636bd2df5bb4fdfab61154b8071221e3
maurypb <maury.rosenfeld@gmail.com>
2024-11-03 10:23:59 -0600

    another little change.

--- champions_classes.py
+++ champions_classes.py
@@ -3,7 +3,7 @@ import logging
 import re
 
 # a meaningless comment here.
-# a third meaningless comment here...
+# a third meaningless comment here..
 
 class ChampionModel:
     def __init__(self, epoch, loss, filename):
